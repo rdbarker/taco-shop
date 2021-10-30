0a051439c7e21096279b78f703b5f500e24baf80
rdbarker <ryan.dean.barker@gmail.com>
2021-10-29 20:50:57 -0400

    added text content to create element

--- src/components/create-element/create-element.js
+++ src/components/create-element/create-element.js
@@ -1,7 +1,8 @@
-const createElement = (tag,parentSelector="body",cssClasses="",source="") => {
+const createElement = (tag,parentSelector="body",text="",cssClasses="",source="") => {
     const element = document.createElement(tag);
     if (cssClasses) element.classList = cssClasses;
     if (source) element.src = source;
+    if (text) element.textContent = text;
     const parent = document.querySelector(parentSelector);
     parent.appendChild(element);
     return element
