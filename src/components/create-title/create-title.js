55a1f6d91b1a5792e90d0bfb677d44f981c5fe40
rdbarker <ryan.dean.barker@gmail.com>
2021-10-30 12:16:39 -0400

    create-title component doesn't seem necessary

--- src/components/create-title/create-title.js
+++
@@ -1,9 +0,0 @@
-import createElement from '../create-element/create-element.js';
-import './create-title.css';
-
-const title = createElement({tag:"h1",inner:"Tacos del JS",parent:"#container",css:"page-title"})
-const menuContainer = createElement({parent:"#container",css:"menu-container"})
-const menuList = createElement({tag:"ul",parent:menuContainer})
-const home = createElement({tag:"li", parent:menuList,inner:"Home"})
-const menu = createElement({tag:"li", parent:menuList,inner:"Menu"})
-const contact = createElement({tag:"li",parent:menuList,inner:"Contact"})
