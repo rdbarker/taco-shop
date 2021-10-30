527c018444bd09a5a46a55bffe2e9a402747a747
rdbarker <ryan.dean.barker@gmail.com>
2021-10-30 00:37:45 -0400

    basic css for title component

--- src/components/create-title/create-title.js
+++ src/components/create-title/create-title.js
@@ -1,7 +1,7 @@
 import createElement from '../create-element/create-element.js';
 import './create-title.css';
 
-const title = createElement({tag:"h1",inner:"Tacos del JS",parent:"#container"})
+const title = createElement({tag:"h1",inner:"Tacos del JS",parent:"#container",css:"page-title"})
 const menuContainer = createElement({parent:"#container",css:"menu-container"})
 const menuList = createElement({tag:"ul",parent:menuContainer})
 const home = createElement({tag:"li", parent:menuList,inner:"Home"})
