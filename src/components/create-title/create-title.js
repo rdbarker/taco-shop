ff2c48b2e27848b35c420da4409ddb4d2250cbea
rdbarker <ryan.dean.barker@gmail.com>
2021-10-30 00:19:55 -0400

    created title module

---
+++ src/components/create-title/create-title.js
@@ -0,0 +1,8 @@
+import createElement from '../create-element/create-element';
+
+const title = createElement({tag:"h1",inner:"Tacos del JS",parent:"#container"})
+const menuContainer = createElement({parent:"#container",css:"menu-container"})
+const menuList = createElement({tag:"ul",parent:menuContainer})
+const home = createElement({tag:"li", parent:menuList,inner:"Home"})
+const menu = createElement({tag:"li", parent:menuList,inner:"Menu"})
+const contact = createElement({tag:"li",parent:menuList,inner:"Contact"})
